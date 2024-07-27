936bcb09802ece3032c6911dc7406f5a7082ba7d
Zhaohui Mei <mzh.whut@gmail.com>
2024-07-28 01:25:04 +0800

    Automatically submit code on 20240728_01:25:03

--- docs/public/scripts/python/test_redis_cluster.py
+++ docs/public/scripts/python/test_redis_cluster.py
@@ -22,7 +22,7 @@ rc = RedisCluster(startup_nodes=startup_nodes,
                   password=password)
 
 # 使用Redis集群
-rc.set("foo", "barbar")
+rc.set("foo", "bar")
 print(rc.get("foo"))
 print(rc.get("name"))
 print(rc.get("num"))
